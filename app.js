2a5afac173aab4f5a8f5c6bb0839498cc226c0a1
Agostina Romero <agostinaromontamatt@gmail.com>
2023-04-03 09:01:06 -0300

    mandatoryResolved

--- app.js
+++ app.js
@@ -12,14 +12,31 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
+hbs.registerPartials("partials_absolute_path") //
 
-// ...
+app.get('/', (req, res) => res.render('index'));
 
-// Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.render('beers', { beers: beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (request, response, next) => {
+
+  punkAPI.getRandom()
+    .then(randomArr => {
+      const randomData = {
+        randomBeer: randomArr[0]
+      }
+      response.render('random-beer', randomData);
+    })
+  .catch();
+
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
